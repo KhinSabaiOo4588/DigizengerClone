4176d044a24d61daadecaaa82afaa066b93aaf01
KhinSabaiOo4588 <khinsabaioo@edusn.co.uk>
2024-09-13 20:40:56 +0630

    Already Study About React

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,25 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//JSX
-// function Greeting(){
-//   // return <h1>Hello Nice To meet you React</h1>//JSX
-//   return(
-//     <div>
-//       <h1>This is me and Nice To meet you React</h1>
-//     </div>
-//   );
-// }
+// JSX
+//always return single element
+// use div/ section/ article or fragment<></>
+// use camelCase for html attribute
+// use ClassName instead of class
+// close every Element
+// formatting
 
-
-// React Such as Know
 function Greeting(){
-  // return React.createElement("h1",{},"Hello World");
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1",{},"Hello World")
+  // return <h1>Hello Nice To meet you React</h1>//JSX
+  return(
+    <div>
+      <div>
+        <h1>This is me and Nice To meet you React</h1>
+      </div>
+      <div>
+        <p>This is use single element example.</p>
+      </div>
+    </div>  
   );
 }
+
+
+// // React Such as Know
+// function Greeting(){
+//   // return React.createElement("h1",{},"Hello World");
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1",{},"Hello World")
+//   );
+// }
 
 ReactDOM.render(<Greeting/>, document.getElementById("root"));
