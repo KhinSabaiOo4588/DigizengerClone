9df939d0980af62cad8527f03f7498e69bbcee71
KhinSabaiOo4588 <khinsabaioo@edusn.co.uk>
2024-09-18 11:09:26 +0630

    useRef with form submit example

--- use-ref-react/src/App.js
+++ use-ref-react/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+//does not trigger re-render
+//target DOM Nodes/elements -> most used case
 
 function App() {
+
+  const [text,setText ]= useState("");
+  const inputRef = useRef(null);
+
+  function handleForm(e){
+    e.preventDefault();
+    setText(inputRef.current.value);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>UseRef</h1>
+      <form onSubmit={handleForm}>
+        <input 
+          type='text' 
+          ref={inputRef} 
+          placeholder='Enter Your Text'/>
+        <button type='submit'>Submit</button>
+      </form>
+      <h4>{text}</h4>
     </div>
   );
 }
